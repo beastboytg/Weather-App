da018e655748ba0403991818caa3611916e91706
Tanmay <tanmaygupta902636@gmail.com>
2026-06-10 19:15:32 +0530

    setted up the app initially

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+});
+
+app.listen(port , ()=>{
+    console.log (`app running on port : ${port}`);
+});
